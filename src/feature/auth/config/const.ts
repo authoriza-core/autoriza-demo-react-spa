970e9246caa9bac93fa21a468446c11d5b87b28a
Alex Kalinin <alexkalinin.ru@gmail.com>
2026-05-19 01:36:15 +0700

    Add env variables

--- src/feature/auth/config/const.ts
+++ src/feature/auth/config/const.ts
@@ -5,5 +5,7 @@ export const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI
   || 'http://localhost:5173/callback';
 
 /**  Редко изменяющиеся настройки. В большинстве случаев можно оставить как есть. */
-export const OIDC_PROVIDER = 'https://oidc.authoriza.ru/oidc';
-export const SCOPE = 'openid offline_access email profile';
+export const OIDC_PROVIDER = import.meta.env.VITE_OIDC_PROVIDER
+  || 'https://oidc.authoriza.ru/oidc';
+export const SCOPE = import.meta.env.VITE_SCOPE
+  || 'openid offline_access email profile';
--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,8 +1,12 @@
 /// <reference types="vite/client" />
 
+import { SCOPE } from './src/feature/auth/config/const.ts';
+
 interface ImportMetaEnv {
   readonly VITE_CLIENT_ID: string;
   readonly VITE_REDIRECT_URI: string;
+  readonly VITE_OIDC_PROVIDER: string;
+  readonly VITE_SCOPE: string;
 }
 
 interface ImportMeta {
